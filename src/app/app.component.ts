edebb00b7af83ac3b26a9363df3a559eb036f203
Richard HeRo <alexander.01093@gmail.com>
2023-07-19 17:45:13 -0500

    set dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'academy';
   ngOnInit(): void {
-    
+    window.localStorage.setItem('data-theme', 'light');
   }
 }
