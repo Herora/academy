7cb549e2509bc2fd9817bece319647a243bd0367
Richard HeRo <alexander.01093@gmail.com>
2023-07-19 18:54:09 -0500

    login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'academy';
+  public modeTheme : String | null = window.localStorage.getItem('data-theme');
   ngOnInit(): void {
-    window.localStorage.setItem('data-theme', 'light');
+    if(this.modeTheme === null){
+      window.localStorage.setItem('data-theme', 'light');
+    }
   }
 }
--- src/app/views/account/pages/login/login.component.ts
+++ src/app/views/account/pages/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {  Tab, Ripple , Input, initTE} from "tw-elements";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  public modeTheme : String | null = window.localStorage.getItem('data-theme');
+  ngOnInit(): void { 
+    initTE({ Tab, Ripple, Input });
+  }
+  public dataRouters = [
+    {name : 'Inicio',url:'/account/initial'},
+    {name : 'login',url:'/account/login'},
+    {name : 'registro',url:'/account/register'}
+  ]
+  thememode(data : String){
+    this.modeTheme = data;
+  }
 }
