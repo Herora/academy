161ecb2939b93f6c20035f6c2e135c509ac0fa09
Richard HeRo <alexander.01093@gmail.com>
2023-07-22 12:13:55 -0500

    color and carrusel

--- src/app/shareds/carrusel/carrusel.component.ts
+++ src/app/shareds/carrusel/carrusel.component.ts
@@ -1,15 +1,31 @@
+
+
 import { Component, OnInit } from '@angular/core';
-import {
-  Carousel,
-  initTE,
-} from "tw-elements"
+
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
   styleUrls: ['./carrusel.component.scss']
 })
 export class CarruselComponent implements OnInit {
+  slider:any
+  defaultTransform:any
+   goNext() {
+    this.defaultTransform = this.defaultTransform - 398;
+    if (Math.abs(this.defaultTransform) >= this.slider.scrollWidth / 1.7) this.defaultTransform = 0;
+    this.slider.style.transform = "translateX(" + this.defaultTransform + "px)";
+}
+ goPrev() {
+   
+    if (Math.abs(this.defaultTransform) === 0) this.defaultTransform = 0;
+    else this.defaultTransform = this.defaultTransform + 398;
+    this.slider.style.transform = "translateX(" + this.defaultTransform + "px)";
+}
+  constructor() { }
+
   ngOnInit(): void {
-    initTE({ Carousel })
+    this.slider = document.getElementById("slider");
+    this.defaultTransform=0
   }
+
 }
